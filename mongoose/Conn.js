77242bb1da39178bf3e38f1fc041f09699813634
Goru-98 <gourav98134@gmail.com>
2023-07-25 12:25:56 +0530

    mongodb atlas included

--- mongoose/Conn.js
+++ mongoose/Conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(`mongodb://127.0.0.1:27017/portfolio`).then(()=>{
+mongoose.connect(`mongodb+srv://goru2k18:gourav$1234@cluster0.m643iuo.mongodb.net/portf?retryWrites=true&w=majority`).then(()=>{
     console.log("connection is successful")
 
 }).catch((e)=>{
